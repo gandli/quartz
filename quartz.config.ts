fad925d03310eb2b98ea31f03773f011cb0c2acf
gandli <gandli@qq.com>
2024-04-09 18:01:00 +0800

    📝 docs: 调整了`quartz.config.ts`中`Plugin.CreatedModifiedDate`的优先级顺序，将`git`提前到了`filesystem`之前

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
+        priority: ["frontmatter", "filesystem", "git"],
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
