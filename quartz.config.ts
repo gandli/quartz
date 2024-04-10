f299147f5f5d635942c191725055ecf48f595baf
gandli <gandli@qq.com>
2024-04-10 09:36:56 +0800

    📝 docs: 调整默认日期类型为"modified"以反映最近修改，以便更好地跟踪更新记录

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "zh-CN",
     baseUrl: "blog.chenxuexin.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created", //modified、created
+    defaultDateType: "modified", //modified、created
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
