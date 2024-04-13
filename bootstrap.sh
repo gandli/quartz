7b77fd35da508b3f1a8e3c33e9f6491d2bee90e0
gandli <gandli@qq.com>
2024-04-14 01:33:44 +0800

    ✨ feat: 添加了一个新的bash脚本`bootstrap.sh`，根据指定的团队来设置git sparse-checkout以仅包含相应的文件夹

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+SCRIPTNAME=$0
+die() {
+	echo "$SCRIPTNAME: $1"
+	exit 1
+}
+
+TEAM=$1
+
+case $TEAM in
+"content")
+	FOLDERS="content"
+	;;
+*)
+	die "please specify a valid team"
+	;;
+esac
+
+echo "Running 'git sparse-checkout init --cone'"
+git sparse-checkout init --cone
+
+echo "Running 'git sparse-checkout set $FOLDERS'"
+git sparse-checkout set $FOLDERS
